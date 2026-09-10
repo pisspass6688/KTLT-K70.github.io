c5733e9de79a70cb3789aca7dde10280422df8b3
KenZ <161334755+pisspass6688@users.noreply.github.com>
2026-09-10 21:54:00 +0700

    Update ktltbai04.cpp

--- ktltbai04.cpp
+++ ktltbai04.cpp
@@ -16,28 +16,30 @@ using namespace std;
 int main()
 {
     //Khai báo
-    float x, i = 0, s;
-        
-        //Giao diện nhập
-        printf("=== Chương trình tính sinx với độ chính xác 10^-4 ===\n\n");
-        printf("Hãy nhập x cho công thức sau:\nsinx = x - x^3/3! + x^5/5! + ... + (-1)^n*x^(2*n+1)/(2*n+1)! + ...\n\n");
-        printf("(Giới hạn chương trình x = 58)Nhập x = ");
-	scanf("%f", &x);
-        
-	s = x;
-	tg = x;
-        
-        do
-        {
-	    i += 1;
-	    tg = tg*x*x/((2*n)*(2*n+1));
-	    s += tg;
-        }
-        while(fabs(s) >= 0.0001);
-		printf("sin(%0.1f) = %0.1f\n", x, sinx);
+    float x, i = 0, s, tg;
 
-    cout<<endl;
+    //Giao diện nhập
+    printf("=== Chương trình tính sinx với độ chính xác 10^-4 ===\n\n");
+    printf("Hãy nhập x cho công thức sau:\nsinx = x - x^3/3! + x^5/5! + ... + (-1)^n*x^(2*n+1)/(2*n+1)! + ...\n\n");
+    printf("(Giới hạn chương trình x = 58) Nhập x = ");
+    scanf("%f", &x);
+
+    s = x;
+    tg = x;
+
+    do
+    {
+        i += 1;
+        tg = -tg * x * x / ((2 * i) * (2 * i + 1));
+        s += tg;
+    }
+    while(fabs(tg) >= 0.0001);
+
+    printf("sin(%0.1f) = %0.4f\n", x, s);
+
+    cout << endl;
     return 0;
 }
+
 //===Dinh nghia ham===
 
