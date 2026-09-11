060c8e50844073ee80aade7ade6cd564a4360ff9
Nguyễn Phụ Mạnh <SV7060323@localhost.localdomain>
2026-09-11 19:22:32 +0700

    ManhKenZ add file

--- ktltbai03.cpp
+++ ktltbai03.cpp
@@ -13,18 +13,25 @@ using namespace std;
 //===chuong trinh chinh===
 int main()
 {
+    //Khai Báo Biến
     int n,i,gt = 1,Tong = 0;
     
+    //Giao diện nhập giới hạn cuối của tổng
     printf("\n\nChương trình tính tổng Q = 1.1! + 2.2! +.. + n.n!\n");
     printf("Nhập n = ");
     scanf("%d", &n);
     
+    //Vòng lặp tính tổng
     for(int i = 1; i <= n; i++)
     {
+	//Tính giai thừa i!
         gt = gt * i;
+        
+        //Cộng số hạng i.i! vào tổng
         Tong = Tong + i * gt;
     }
     
+    //Write
     printf("\n\nTổng = %d\n", Tong);
 
     cout<<endl;
