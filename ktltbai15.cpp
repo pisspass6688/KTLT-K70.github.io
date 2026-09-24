b62e5b7e6bee6adc5d72cbf45111257adac55fd1
KenZ <161334755+pisspass6688@users.noreply.github.com>
2026-09-24 16:01:52 +0700

    update

--- ktltbai15.cpp
+++ ktltbai15.cpp
@@ -24,12 +24,22 @@ int main()
     printf("\n\nNhập vào số lượng tên: ");
     scanf("%d",&n);
 
+    //Kiểm tra số lượng tên
+    if(n<1 || n>100)
+    {
+        printf("\nSố lượng tên không hợp lệ!");
+        printf("\nSố lượng tên phải từ 1 đến 100.");
+
+        cout<<endl;
+        return 0;
+    }
+
     //Nhập vào danh sách tên
     printf("\nNhập vào danh sách tên:\n");
     for(i=0;i<n;i++)
     {
         printf("Tên thứ %d: ",i+1);
-        scanf("%s",a[i]);
+        scanf("%29s",a[i]);
     }
 
     //Sắp xếp danh sách tên theo vần ABC
@@ -37,6 +47,7 @@ int main()
         for(j=i+1;j<n;j++)
             if(strcmp(a[i],a[j])>0)
             {
+                //Đổi chỗ hai xâu
                 strcpy(tg,a[i]);
                 strcpy(a[i],a[j]);
                 strcpy(a[j],tg);
@@ -45,11 +56,10 @@ int main()
     //Đưa ra danh sách tên sau khi sắp xếp
     printf("\nDanh sách tên sau khi sắp xếp theo vần ABC:\n");
     for(i=0;i<n;i++)
-        printf("%s\n",a[i]);
+        printf("%d. %s\n",i+1,a[i]);
 
     cout<<endl;
     return 0;
 }
-
 //===định nghĩa hàm===
 
